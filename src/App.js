d312fe9e69715c5f14f7f9dfb4f7e0570a65351a
Job van den Hoonaard <jobvandenhoonaard@Jobs-MacBook-Pro.local>
2019-10-22 15:35:38 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {
+        voorletters: undefined,
+        tussenvoegsel: undefined,
+        achternaam: undefined,
+        postcode: undefined,
+        straatnaam: undefined,
+        stad: undefined,
+        huisnummer: undefined,
+        email: undefined,
+      },
+    }
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <GlobalStyle />
+        <h1> Incetro Form</h1>
+        <form>
+
+        </form>
+      </Wrapper>
+    );
+  }
+
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'IBM Plex Sans', sans-serif;
+}
+
+h1 {
+  font-size: 3em;
+  font-weight: 700;
+}
+
+h2 {
+  font-size: 1em;
+  font-weight: 700;
+}
+
+p {
+  font-size: 1em;
+  font-weight: 400;
+}
+`
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  padding: 1em;
+  max-width: 640px;
+`;
